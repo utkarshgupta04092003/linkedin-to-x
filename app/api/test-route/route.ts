d80b98b04e14e06b810973b710e78acdeb13dc08
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2024-11-24 14:55:47 +0530

    Add Metadata Table in test-route

--- app/api/test-route/route.ts
+++ app/api/test-route/route.ts
@@ -1,6 +1,22 @@
+import { prisma } from "@/app/_lib/utils/globals";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   console.log("TEST CRON is running fine.");
-  return NextResponse.json({ message: "Hello World!" });
+  const jobData = await prisma.jobs.findMany();
+  const similarJobsUpdated = jobData.filter((job) => job.isSimilarJobsUpdated);
+  const xPostUpdated = jobData.filter((job) => job.isXPosted);
+  await prisma.metaData.create({
+    data: {
+      totalJobs: jobData.length,
+      totalXPostedJobs: xPostUpdated.length,
+      totalSimilarJobsUpdated: similarJobsUpdated.length,
+    },
+  });
+  return NextResponse.json({
+    message: "Hello World!",
+    totalJobs: jobData.length,
+    totalXPostedJobs: xPostUpdated.length,
+    isScrapedJobTrue: similarJobsUpdated.length,
+  });
 }
