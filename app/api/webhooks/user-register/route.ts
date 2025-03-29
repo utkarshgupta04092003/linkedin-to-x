3acc1598cb7d442e1fce7d86b3cd7aaf3493fb23
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2025-03-29 18:45:59 +0000

    Update Webhook for Update event & Update User Schema

--- app/api/webhooks/user-register/route.ts
+++ app/api/webhooks/user-register/route.ts
@@ -34,30 +34,45 @@ export async function POST(req: Request) {
         )
     }
     const eventType = evt.type
-    // Handling 'user.created' event
-    if (eventType === "user.created") {
+    if (eventType === "user.created" || eventType === "user.updated") {
+        const { email_addresses, primary_email_address_id } = evt.data
+        const primaryEmail = email_addresses.find((email) => email.id === primary_email_address_id)
+        if (!primaryEmail) {
+            return NextResponse.json({ message: "No primary email found" }, { status: 400 })
+        }
         try {
-            const { email_addresses, primary_email_address_id } = evt.data
-            const primaryEmail = email_addresses.find(
-                (email) => email.id === primary_email_address_id
-            )
-            if (!primaryEmail) {
-                return NextResponse.json({ message: "No primary email found" }, { status: 400 })
-            }
-            await prisma.user.create({
-                data: {
+            const user = await prisma.user.upsert({
+                where: { clerkUserId: evt.data.id },
+                update: {
+                    email: primaryEmail.email_address,
+                    emailVerified: primaryEmail.verification?.status === "verified",
+                    firstName: evt.data.first_name ?? "",
+                    lastName: evt.data.last_name ?? "",
+                    imageUrl: evt.data.image_url,
+                    userType: UserType.Standard,
+                    updatedAt: new Date(),
+                },
+                create: {
                     clerkUserId: evt.data.id!,
                     email: primaryEmail.email_address,
-                    firstName: evt.data.first_name ?? "firstName",
-                    lastName: evt.data.last_name ?? "lastName",
+                    emailVerified: primaryEmail.verification?.status === "verified",
+                    firstName: evt.data.first_name ?? "",
+                    lastName: evt.data.last_name ?? "",
+                    imageUrl: evt.data.image_url,
                     userType: UserType.Standard,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
             })
+            return NextResponse.json(
+                { message: "Webhook received successfully", data: user },
+                { status: 200 }
+            )
         } catch (error) {
-            return NextResponse.json({ message: "Error creating user" }, { status: 500 })
+            return NextResponse.json(
+                { message: `Error creating user for ${primaryEmail.email_address}` },
+                { status: 500 }
+            )
         }
     }
-    return NextResponse.json({ message: "Webhook received successfully" }, { status: 200 })
 }
